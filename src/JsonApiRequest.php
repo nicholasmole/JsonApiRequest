65870618546d7f738c8f5f5244611b817ec37e2e
Nick Mole <nmole065@g.rwu.edu>
2019-01-13 00:59:42 -0500

    [feature] Make Json Api Request
    Create the JsonApiRequest Class.
    
    JsonApiRequest uses the php Client URL Library.

---
+++ src/JsonApiRequest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+/**
+ * JsonApiRequest
+ * 
+ * Use php Curl Library for REST API requests.
+ * @author Nicholas Mole
+ */
+final class JsonApiRequest
+{
+    /**
+     * @param string $result - REST API result
+     */
+    private $result;
+
+    /**
+     * Constructor
+     * Requires request rest api url
+     * 
+     * @param string $request - REST API result
+     */
+    public function __construct(string $request)
+    {
+        $this->ensureIsValidRequestInput($request);
+
+        $this->result = $this->getJsonResponse($request);
+
+    }
+
+    /**
+     * ensureIsValidRequestInput
+     * Validates constructor input as string
+     * 
+     * @param string $request - REST API result
+     */
+    private function ensureIsValidRequestInput(string $request): void
+    {
+        if (!is_string($request)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid input type string',
+                    $request
+                )
+            );
+        }
+    }
+
+    /**
+     * getJsonResponse
+     * Get the json response.
+     * Stores as $result in constructor
+     * 
+     * Upon failure curl_exec() returns false.
+     * Convert false to string response.
+     * 
+     * @param string $request - REST API result
+     */
+    private function getJsonResponse(string $request): string
+    {
+        $curl_session = curl_init();
+        curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl_session, CURLOPT_URL, $request);
+        $result = curl_exec($curl_session);
+        curl_close($curl_session);
+
+        if ($result === false) {
+            return 'Curl request failed. Please check API route';
+        }
+        return $result;
+    }
+
+    /**
+     * getResult
+     * Get the result from JsonApiRequest Curl request
+     * 
+     */
+    public function getResult(): string
+    {
+        return $this->result;
+    }
+}
